0371986e5f26c54e5561ba55e16ce1eb028f6b0a
LoickDev97438 <loickcherimont@gmail.com>
2022-03-25 10:30:43 +0400

    Append Description key in the main struct

--- main.go
+++ main.go
@@ -7,18 +7,19 @@ import (
 )
 
 type Cards struct {
-	Image    string
-	AltImage string
-	City     string
-	Country  string
-	Stars    float64
+	Image       string
+	AltImage    string
+	City        string
+	Country     string
+	Stars       float64
+	Description string
 }
 
 // Prepare data for HTML template
 var data = []Cards{
-	{"sanfrancisco.jpg", "Golden Gate Bridge", "San Francisco", "USA", 4},
-	{"paris.jpg", "Eiffel Tower", "Paris", "France", 4.5},
-	{"auckland.jpg", "Auckland downtown", "Auckland", "New Zealand", 4.75},
+	{"sanfrancisco.jpg", "Golden Gate Bridge", "San Francisco", "USA", 4, "Lorem ipsum"},
+	{"paris.jpg", "Eiffel Tower", "Paris", "France", 4.5, "Lorem ipsum"},
+	{"auckland.jpg", "Auckland downtown", "Auckland", "New Zealand", 4.75, "Lorem ipsum"},
 }
 
 // Generate the template
