4734e49d68cf8f061debfc79128debb124bd577e
LoickDev97438 <loickcherimont@gmail.com>
2022-04-26 15:49:07 +0400

    Search handler for specific city chosen by user

--- main/main.go
+++ main/main.go
@@ -15,8 +15,9 @@ func main() {
 	// Handle index page with styles
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs)) // (2)
-	mux.HandleFunc("/travel", handler.GetIndexPage)
 
+	mux.HandleFunc("/search", handler.SearchHandler)
+	mux.HandleFunc("/travel", handler.GetIndexPage)
 	// Serve on 8080 PORT
 	fmt.Println("Listening and serving on PORT 8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
